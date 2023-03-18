5f6a7f6d46d099df155a8f7da6dc98f7aff60bf4
sechiyo97 <sechiyo97@daum.net>
2023-03-18 23:29:53 +0900

    add examples for 22~26

---
+++ RowColDemo/app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.rowcoldemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.rowcoldemo.ui.theme.RowColDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RowColDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Row(
+        modifier = Modifier.size(width = 400.dp, height = 200.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.End
+    ) {
+        TextCell("1")
+        TextCell("2")
+        TextCell("3")
+    }
+}
+
+@Composable
+fun TextCell(text: String, modifier: Modifier = Modifier) {
+    val cellModifier = Modifier
+        .padding(4.dp)
+        .size(100.dp, 100.dp)
+        .border(width = 4.dp, color = Color.Black)
+
+    Text(
+        text = text,
+        modifier = cellModifier.then(modifier),
+        fontSize = 70.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RowColDemoTheme {
+        MainScreen()
+    }
+}
---
+++ SlotApiDemo/app/src/main/java/com/example/slotapidemo/MainActivity.kt
@@ -0,0 +1,126 @@
+package com.example.slotapidemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.slotapidemo.ui.theme.SlotApiDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SlotApiDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    var linearSelected by remember { mutableStateOf(true) }
+    var imageSelected by remember { mutableStateOf(true) }
+    val onLinearClick = { value: Boolean ->
+        linearSelected = value
+    }
+    val onTitleClick = { value: Boolean ->
+        imageSelected = value
+    }
+    ScreenContent(
+        linearSelected = linearSelected,
+        imageSelected = imageSelected,
+        onTitleClick = onTitleClick,
+        onLinearClick = onLinearClick,
+        titleContent = {
+            if (imageSelected) {
+                TitleImage(drawing = R.drawable.ic_baseline_cloud_download_24)
+            } else {
+                Text(
+                    "Downloading",
+                    style = MaterialTheme.typography.h3,
+                    modifier = Modifier.padding(30.dp)
+                )
+            }
+        },
+        progressContent = {
+            if (linearSelected) {
+                LinearProgressIndicator(Modifier.height(40.dp))
+            } else {
+                CircularProgressIndicator(Modifier.size(200.dp), strokeWidth = 18.dp)
+            }
+        }
+    )
+}
+
+@Composable
+fun ScreenContent(
+    linearSelected: Boolean,
+    imageSelected: Boolean,
+    onTitleClick: (Boolean) -> Unit,
+    onLinearClick: (Boolean) -> Unit,
+    titleContent: @Composable () -> Unit,
+    progressContent: @Composable () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        titleContent()
+        progressContent()
+        CheckBoxes(
+            linearSelected = linearSelected,
+            imageSelected = imageSelected,
+            onTitleClick = onTitleClick,
+            onLinearClick = onLinearClick
+        )
+    }
+}
+
+@Composable
+fun CheckBoxes(
+    linearSelected: Boolean,
+    imageSelected: Boolean,
+    onTitleClick: (Boolean) -> Unit,
+    onLinearClick: (Boolean) -> Unit
+) {
+    Row(
+        modifier = Modifier.padding(20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(checked = imageSelected, onCheckedChange = onTitleClick)
+        Text("Image Title")
+        Spacer(modifier = Modifier.width(20.dp))
+        Checkbox(checked = linearSelected, onCheckedChange = onLinearClick)
+        Text("Linear Progress")
+    }
+}
+
+@Composable
+fun TitleImage(drawing: Int) {
+    Image(
+        painter = painterResource(id = drawing),
+        contentDescription = "Title Image"
+    )
+}
+
+@Preview
+@Composable
+fun DemoPreview() {
+    MainScreen()
+}
